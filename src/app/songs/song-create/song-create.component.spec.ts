3d8ab1d307ab886e6ee5189ecbd0bf834b37e281
jorge.pineda@insoftar.com-frontend <89551578+jorgepinedainsoftar@users.noreply.github.com>
2024-03-23 11:36:01 -0500

    Creating components

---
+++ src/app/songs/song-create/song-create.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SongCreateComponent } from './song-create.component';
+
+describe('SongCreateComponent', () => {
+  let component: SongCreateComponent;
+  let fixture: ComponentFixture<SongCreateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SongCreateComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SongCreateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/songs/songs.module.ts
+++ src/app/songs/songs.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SongsComponent } from './songs.component';
+import { SongDetailComponent } from '../song-detail/song-detail.component';
+import { SongCreateComponent } from '../song-create/song-create.component';
 
 
 
 @NgModule({
   declarations: [
-    SongsComponent
+    SongsComponent,
+    SongDetailComponent,
+    SongCreateComponent
   ],
   imports: [
     CommonModule
